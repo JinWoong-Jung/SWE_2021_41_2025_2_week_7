727610ffcce02042a83a57df463b98eba9e78471
JINWOONG JUNG <jinwoong1010@gmail.com>
2025-10-15 18:33:31 +0900

    change main.py

--- main.py
+++ main.py
@@ -6,10 +6,3 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         for j in range(i + 1, length):
             if nums[i] + nums[j] == target:
                 return [i, j]
-
-# Example 1:
-print('Example 1:', twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
-# Example 2:
-print('Example 2:', twoSum([3, 2, 4], 6))  # Output: [1, 2]
-# Example 3:
-print('Example 3:', twoSum([3, 3], 6))  # Output: [0, 1]
